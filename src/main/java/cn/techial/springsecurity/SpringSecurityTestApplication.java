11757b04596b145dc0a487695ba01dc0e9ca7e5b
techial <me@techial.top>
2019-10-24 22:45:58 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- src/main/java/cn/techial/springsecurity/SpringSecurityTestApplication.java
+++ src/main/java/cn/techial/springsecurity/SpringSecurityTestApplication.java
@@ -2,8 +2,13 @@ package cn.techial.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+/**
+ * @author techial
+ */
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SpringSecurityTestApplication {
 
     public static void main(String[] args) {
